8a0f84615919358114b8e875f6a1c9c14ad4f9ee
gatraenggar <70312407+gatraenggar@users.noreply.github.com>
2022-08-17 15:54:12 +0800

    Revert "Feature/friend join room"

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react'
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 import Lobby from './pages/lobby';
-import Navbar from './components/navbar';
-import WaitingRoom from './pages/waitingroom';
-
-const { io } = require("socket.io-client");
-const socket = io("http://localhost:3001");
 
 function App() {
-  const [username, setUsername] = useState("");
-
-  if (username.length >= 3) {
-    return <WaitingRoom socket={socket} username={username} />;
-  }
-
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Lobby
-              socket={socket}
-              username={username}
-              setUsername={(name) => setUsername(name)}
-            />
-          } />
+        <Route path="/" element={<Lobby />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/lobby.jsx
+++ src/pages/lobby.jsx
@@ -1,39 +1,9 @@
-import './../styles/lobby.css'
-import './../styles/colors.css'
+import React from 'react'
 
-export default function Lobby({ socket, setUsername }) {
-    // const [showWarning, setShowWarning] = useState(false)
-
-    const startGame = () => {
-        const username = document.getElementById('username').value;
-        if (username.length < 3) {
-            window.alert("Please input at least 3 characters");
-            return;
-        }
-
-        socket.emit("user-join", { username }, (response) => {
-            console.log(response);
-        });
-
-        setUsername(username);
-    };
-
+export default function Lobby() {
     return (
-        <div className='lobby-container bg-primary'>
-            <div className='login-div'>
-                <div className='title'>
-                    Create user
-                </div>
-                <div className="fields">
-                    <div className="username">
-                        <input id='username' placeholder=' Enter username' className='fieldsInput' />
-                    </div>
-                    {/* <p hidden={showWarning} style={{ color: "red" }}>please enter username</p> */}
-                    <button onClick={() => startGame()} className='signin-button'>
-                        Start Game
-                    </button>
-                </div>
-            </div>
+        <div>
+            hello
         </div>
     )
 }
