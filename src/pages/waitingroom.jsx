539353f949bb9a63723b61fd78cd4422ea28774a
gatraenggar <gatraenggarwicaksono@gmail.com>
2022-08-21 01:11:02 +0800

    fix: missing host functionality

--- src/pages/waitingroom.jsx
+++ src/pages/waitingroom.jsx
@@ -63,9 +63,18 @@ export default function WaitingRoom({
                 </div>
                 {
                     users.length ?
-                        users.map((user) => (
+                        users.map((user, i) => (
                             <div key={user.id} className='player-list'>
-                                {user.username}
+                                {user.username} &nbsp;
+
+                                {
+                                    i === 0 ?
+                                    <span style={{ color: "gold" }}>
+                                        (&#128273; Host)
+                                    </span>
+                                    :
+                                    null
+                                }
                             </div>
                         ))
                         :
@@ -74,16 +83,24 @@ export default function WaitingRoom({
                         </div>
                 }
             </div>
-            {/* ADMIN ONLY */}
-            <div style={{ backgroundColor: "#011d2e", color: "white", fontSize: "28px", padding: "12px" }}>
-                <input type="range" name="" id="" min="1" max="15" defaultValue={8} onChange={(e) => setDuration(e.target.value)} />
-                <span> {duration} minute(s) </span>
-            </div>
-            <button className='start-game-button' onClick={() => startGame()}>
-                Start Game
-            </button>
-            {/* PLAYERS ONLY */}
-            <p className='waiting-room-message'>Waiting for other agents to join...</p>
+            {
+                users.length && users[0].username === username ?
+                <>
+                    {/* HOST ONLY */}
+                    <div style={{ backgroundColor: "#011d2e", color: "white", fontSize: "28px", padding: "12px" }}>
+                        <input type="range" name="" id="" min="1" max="15" defaultValue={8} onChange={(e) => setDuration(e.target.value)} />
+                        <span> {duration} minute(s) </span>
+                    </div>
+                    <button className='start-game-button' onClick={() => startGame()}>
+                        Start Game
+                    </button>
+                </>
+                :
+                <>
+                    {/* PLAYERS ONLY */}
+                    <p className='waiting-room-message'>Waiting host for starting the game...</p>
+                </>
+            }
         </div>
     )
 }
