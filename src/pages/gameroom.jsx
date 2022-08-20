b3384039b01dc36659b58acd8cd85fd2121dc3a3
gatraenggar <gatraenggarwicaksono@gmail.com>
2022-08-20 17:15:39 +0800

    feat: game timer

--- src/pages/gameroom.jsx
+++ src/pages/gameroom.jsx
@@ -1,13 +1,39 @@
 import './../styles/lobby.css'
 import './../styles/colors.css'
+import { unixTimeDiff } from './../util/time'
+import { useEffect, useState } from 'react';
 
-export default function GameRoom({ socket, username, role, place }) {
+export default function GameRoom({ 
+    socket, 
+    username, 
+    role, 
+    place,
+    endTime,
+}) {
+     const [timer, setTimer] = useState(unixTimeDiff(endTime));
+
+    useEffect(() => {
+        const t = setTimeout(() => {
+            setTimer(unixTimeDiff(endTime, new Date().getTime()));
+        }, 1000);
+    
+        return () => clearTimeout(t);
+    });
+
     return (
         <div className='waiting-room-container bg-primary'>
             <div className='waiting-room-div'>
                 <div className='waiting-room-title'>
                     Game room
                 </div>
+
+                <h5 className='waiting-room-title'> 
+                    Time Left 
+                    <br /> 
+                    { Math.floor(timer/60) < 10 ? "0" + Math.floor(timer/60) : Math.floor(timer/60) }
+                    :
+                    { timer % 60 < 10 ? "0" + timer % 60 : timer % 60 }
+                </h5>
 
                 <br /><br />
                 <div className='waiting-room-title'>
